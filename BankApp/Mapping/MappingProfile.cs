6fe9ee7244fb7a6f583d85420857b564c242c9a1
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2020-01-17 18:58:13 +0100

    Add MappingProfile

---
+++ BankApp/Mapping/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BankApp.Dtos;
+using BankApp.Models;
+
+namespace BankApp.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Customer, CustomerDto>();
+            CreateMap<ApplicationUser, ApplicationUserDto>();
+        }
+    }
+}
