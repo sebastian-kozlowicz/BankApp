4a1835c416e9fd4c0507a2e18ab6564adfc9ad41
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2020-02-10 21:19:15 +0100

    Add CardDto

---
+++ BankApp/Dtos/CardDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankApp.Dtos
+{
+    public class CardDto
+    {
+        public int Id { get; set; }
+        public string Number { get; set; }
+    }
+}
