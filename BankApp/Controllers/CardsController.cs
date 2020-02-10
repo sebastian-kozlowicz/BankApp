d6145caed928e871e22c03075d672beaebed795c
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2020-02-10 21:26:06 +0100

    Add CardsController

---
+++ BankApp/Controllers/CardsController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using BankApp.Data;
+using BankApp.Dtos;
+using BankApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BankApp.Controllers
+{
+    public class CardsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public CardsController(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet("api/{controller}")]
+        public IEnumerable<CardDto> GetCards()
+        {
+            var cards = _context.Cards.ToList();
+            return _mapper.Map<List<Card>, List<CardDto>>(cards);
+        }
+    }
+}
