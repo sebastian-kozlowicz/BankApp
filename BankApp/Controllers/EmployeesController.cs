f04c61cebdad2108899e79f8642510bf79ed3c34
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2020-02-12 21:27:22 +0100

    Add EmployeesController

---
+++ BankApp/Controllers/EmployeesController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using BankApp.Data;
+using BankApp.Dtos;
+using BankApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankApp.Controllers
+{
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public EmployeesController(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet("api/[controller]")]
+        public IEnumerable<EmployeeDto> GetEmployees()
+        {
+            var employees = _context.Employees.Include(c => c.ApplicationUser).ToList();
+            return _mapper.Map<List<Employee>, List<EmployeeDto>>(employees);
+        }
+    }
+}
