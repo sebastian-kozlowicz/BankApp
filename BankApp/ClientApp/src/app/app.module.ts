4dc5154da44e2ecfd8f58b2d3a816043ab66463e
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2020-02-14 19:38:32 +0100

    Add account modules and create routing

--- BankApp/ClientApp/src/app/app.module.ts
+++ BankApp/ClientApp/src/app/app.module.ts
@@ -7,19 +7,33 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
+import { AccountComponent } from './account/account.component';
+import { RegistrationComponent } from './account/registration/registration/registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
+    AccountComponent,
+    RegistrationComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
+      {
+        path: '', component: HomeComponent, pathMatch: 'full'
+      },
+      {
+        path: 'account', component: AccountComponent,
+        children: [
+          {
+            path: 'registration', component: RegistrationComponent
+          },
+        ]
+      },
     ])
   ],
   providers: [
