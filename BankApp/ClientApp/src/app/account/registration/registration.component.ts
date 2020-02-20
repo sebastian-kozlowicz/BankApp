598398fc06dcaefbfa73b8a3c175b839639ba4b2
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2020-02-20 20:43:15 +0100

    Create registration form draft

--- BankApp/ClientApp/src/app/account/registration/registration.component.ts
+++ BankApp/ClientApp/src/app/account/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../../services/account.service';
 
 @Component({
   selector: 'app-registration',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
   }
