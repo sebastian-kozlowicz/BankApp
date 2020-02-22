eee4e65dddbae9bac167bede500c3878b7741da8
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2020-02-22 17:50:53 +0100

    Add password matching validation

--- BankApp/ClientApp/src/app/services/account.service.ts
+++ BankApp/ClientApp/src/app/services/account.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +14,19 @@ export class AccountService {
     email: ['', [Validators.required, Validators.email]],
     password: ['', Validators.required],
     confirmPassword: ['', Validators.required]
+  }, {
+    validator: this.IsPasswordMatch
   });
+
+  IsPasswordMatch(fg: FormGroup) {
+    let password = fg.controls['password'];
+    let confirmPassword = fg.controls['confirmPassword'];
+
+    if (confirmPassword.errors == null) {
+      if (password.value == confirmPassword.value)
+        confirmPassword.setErrors(null);
+      else
+        confirmPassword.setErrors({ passwordMismatch: true })
+    }
+  }
 }
