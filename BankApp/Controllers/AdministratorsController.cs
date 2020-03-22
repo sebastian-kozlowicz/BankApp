0e5c2f4260e2f46ba53b63b62aa28e240a097061
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2020-03-22 22:10:31 +0100

    Add AdministratorsController

---
+++ BankApp/Controllers/AdministratorsController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using BankApp.Data;
+using BankApp.Dtos;
+using BankApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BankApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdministratorsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public AdministratorsController(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IEnumerable<AdministratorDto> GetAdministrators()
+        {
+            var administrators = _context.Administrators.Include(c => c.ApplicationUser).ToList();
+            return _mapper.Map<List<Administrator>, List<AdministratorDto>>(administrators);
+        }
+    }
+}
