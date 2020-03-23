9971a2a36751434211c5d739f53e165d03d25b32
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2020-03-23 20:37:09 +0100

    Add ManagersController

---
+++ BankApp/Controllers/ManagersController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using BankApp.Data;
+using BankApp.Dtos;
+using BankApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ManagersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public ManagersController(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IEnumerable<ManagerDto> GetManagers()
+        {
+            var managers = _context.Managers.Include(c => c.ApplicationUser).ToList();
+            return _mapper.Map<List<Manager>, List<ManagerDto>>(managers);
+        }
+    }
+}
