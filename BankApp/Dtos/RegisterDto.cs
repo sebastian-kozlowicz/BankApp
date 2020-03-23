228a0b5ae7f409b7cb46b7b0c95c308420be443c
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2020-03-23 20:11:45 +0100

    Make Name property required in RegisterDto

--- BankApp/Dtos/RegisterDto.cs
+++ BankApp/Dtos/RegisterDto.cs
@@ -4,6 +4,7 @@ namespace BankApp.Dtos
 {
     public class RegisterDto
     {
+        [Required]
         [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
         [Display(Name = "Name")]
         public string Name { get; set; }
