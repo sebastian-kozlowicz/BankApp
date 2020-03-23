3a82c0d24de6b7d3f48d47782117b783e1a87ec4
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2020-03-23 20:28:28 +0100

    Add ManagerDto

--- BankApp/Dtos/CardDto.cs
+++ BankApp/Dtos/CardDto.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace BankApp.Dtos
+﻿namespace BankApp.Dtos
 {
     public class CardDto
     {
---
+++ BankApp/Dtos/ManagerDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BankApp.Dtos
+{
+    public class ManagerDto
+    {
+        public string Id { get; set; }
+        public ApplicationUserDto ApplicationUser { get; set; }
+    }
+}
