3a60c98e5a62eb801d591940bc4b919f9b557397
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2020-04-28 21:40:47 +0200

    Add material inputs for personal information form

--- BankApp/ClientApp/src/app/material/material.module.ts
+++ BankApp/ClientApp/src/app/material/material.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatStepperModule } from '@angular/material/stepper';
+import { MatInputModule } from '@angular/material/input';
 
 const MaterialComponents = [
   MatButtonModule,
-  MatStepperModule
+  MatStepperModule,
+  MatInputModule
 ];
 
 @NgModule({
