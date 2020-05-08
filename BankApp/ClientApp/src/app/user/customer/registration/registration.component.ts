1d6519bea11da7ee067ceeaeb8e916c72e5f00e7
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2020-05-08 20:22:15 +0200

    Add separate form for personal information

--- BankApp/ClientApp/src/app/user/customer/registration/registration.component.ts
+++ BankApp/ClientApp/src/app/user/customer/registration/registration.component.ts
@@ -6,119 +6,122 @@ import { PasswordValidator } from "../../../validators/password-validator";
 import { Register } from "../../../models/register";
 
 @Component({
-    selector: 'app-registration',
-    templateUrl: './registration.component.html',
-    styleUrls: ['./registration.component.css']
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css']
 })
 export class CustomerRegistrationComponent implements OnInit {
 
-    constructor(private fb: FormBuilder, private authService: AuthService, private toastr: ToastrService) { }
+  constructor(private fb: FormBuilder, private authService: AuthService, private toastr: ToastrService) { }
 
-    get getPasswordErrorCount() {
-        let errorCount = 0;
+  get getPasswordErrorCount() {
+    let errorCount = 0;
 
-        if (this.isErrorOccur(this.password, "required"))
-            errorCount++;
-        if (this.isErrorOccur(this.password, "hasNumber"))
-            errorCount++;
-        if (this.isErrorOccur(this.password, "hasCapitalCase"))
-            errorCount++;
-        if (this.isErrorOccur(this.password, "hasLowerCase"))
-            errorCount++;
-        if (this.isErrorOccur(this.password, "hasSpecialCharacter"))
-            errorCount++;
-        if (this.isErrorOccur(this.password, "minlength"))
-            errorCount++;
+    if (this.isErrorOccur(this.password, "required"))
+      errorCount++;
+    if (this.isErrorOccur(this.password, "hasNumber"))
+      errorCount++;
+    if (this.isErrorOccur(this.password, "hasCapitalCase"))
+      errorCount++;
+    if (this.isErrorOccur(this.password, "hasLowerCase"))
+      errorCount++;
+    if (this.isErrorOccur(this.password, "hasSpecialCharacter"))
+      errorCount++;
+    if (this.isErrorOccur(this.password, "minlength"))
+      errorCount++;
 
-        return errorCount;
+    return errorCount;
+  }
+
+  setErrorClasses() {
+    let errorCount = this.getPasswordErrorCount;
+
+    return {
+      'one-error': errorCount == 1,
+      'two-errors': errorCount == 2,
+      'three-errors': errorCount == 3,
+      'four-errors': errorCount == 4
+    };
+  }
+
+  get name() {
+    return this.personalInformationForm.get('name');
+  }
+  get surname() {
+    return this.personalInformationForm.get('surname');
+  }
+  get email() {
+    return this.personalInformationForm.get('email');
+  }
+  get password() {
+    return this.registerFormModel.get('password');
+  }
+  get confirmPassword() {
+    return this.registerFormModel.get('confirmPassword');
+  }
+
+  personalInformationForm = this.fb.group({
+    name: ['', Validators.required],
+    surname: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]]
+  });
+
+  registerFormModel = this.fb.group({
+    password: ['', Validators.compose([
+      Validators.required,
+      PasswordValidator.patternValidator(/\d/, { hasNumber: true }),
+      PasswordValidator.patternValidator(/[A-Z]/, { hasCapitalCase: true }),
+      PasswordValidator.patternValidator(/[a-z]/, { hasLowerCase: true }),
+      PasswordValidator.patternValidator(/[-!$%^&*()_+|~=`{}\[\]:\/;<>?,.@#]/, { hasSpecialCharacter: true }),
+      Validators.minLength(8)])
+    ],
+    confirmPassword: ['', Validators.required]
+  }, {
+    validator: this.isPasswordMatch
+  });
+
+  isErrorOccur(fg: AbstractControl, error) {
+    return fg.errors?.[error];
+  }
+
+  isPasswordMatch(fg: FormGroup) {
+    let password = fg.controls['password'];
+    let confirmPassword = fg.controls['confirmPassword'];
+
+    if (confirmPassword.errors == null) {
+      if (password.value == confirmPassword.value)
+        confirmPassword.setErrors(null);
+      else
+        confirmPassword.setErrors({ passwordMismatch: true })
     }
+  }
 
-    setErrorClasses() {
-        let errorCount = this.getPasswordErrorCount;
+  ngOnInit(): void {
+  }
 
-        return {
-            'one-error': errorCount == 1,
-            'two-errors': errorCount == 2,
-            'three-errors': errorCount == 3,
-            'four-errors': errorCount == 4
-        };
-    }
+  onSubmit() {
+    let registerModel: Register = {
+      name: this.name.value,
+      surname: this.surname.value,
+      email: this.email.value,
+      password: this.password.value,
+    };
 
-    get name() {
-        return this.registerFormModel.get('name');
-    }
-    get surname() {
-        return this.registerFormModel.get('surname');
-    }
-    get email() {
-        return this.registerFormModel.get('email');
-    }
-    get password() {
-        return this.registerFormModel.get('password');
-    }
-    get confirmPassword() {
-        return this.registerFormModel.get('confirmPassword');
-    }
-
-    registerFormModel = this.fb.group({
-        name: ['', Validators.required],
-        surname: ['', Validators.required],
-        email: ['', [Validators.required, Validators.email]],
-        password: ['', Validators.compose([
-            Validators.required,
-            PasswordValidator.patternValidator(/\d/, { hasNumber: true }),
-            PasswordValidator.patternValidator(/[A-Z]/, { hasCapitalCase: true }),
-            PasswordValidator.patternValidator(/[a-z]/, { hasLowerCase: true }),
-            PasswordValidator.patternValidator(/[-!$%^&*()_+|~=`{}\[\]:\/;<>?,.@#]/, { hasSpecialCharacter: true }),
-            Validators.minLength(8)])
-        ],
-        confirmPassword: ['', Validators.required]
-    }, {
-        validator: this.isPasswordMatch
-    });
-
-    isErrorOccur(fg: AbstractControl, error) {
-        return fg.errors?.[error];
-    }
-
-    isPasswordMatch(fg: FormGroup) {
-        let password = fg.controls['password'];
-        let confirmPassword = fg.controls['confirmPassword'];
-
-        if (confirmPassword.errors == null) {
-            if (password.value == confirmPassword.value)
-                confirmPassword.setErrors(null);
-            else
-                confirmPassword.setErrors({ passwordMismatch: true })
+    this.authService.registerCustomer(registerModel).subscribe(
+      response => {
+        if (response) {
+          this.toastr.success('New user created!', 'Registration successful.');
         }
-    }
-
-    ngOnInit(): void {
-    }
-
-    onSubmit() {
-        let registerModel: Register = {
-            name: this.name.value,
-            surname: this.surname.value,
-            email: this.email.value,
-            password: this.password.value,
-        };
-
-        this.authService.registerCustomer(registerModel).subscribe(
-            response => {
-                if (response) {
-                    this.toastr.success('New user created!', 'Registration successful.');
-                }
-            },
-            badRequest => {
-                if (Array.isArray(badRequest.error))
-                    badRequest.error.forEach(element => {
-                        if (element.code == 'DuplicateUserName')
-                            this.toastr.error('Username is already taken', 'Registration failed.');
-                    });
-                else
-                    this.toastr.error('Registration failed.');
-            }
-        );
-    }
+      },
+      badRequest => {
+        if (Array.isArray(badRequest.error))
+          badRequest.error.forEach(element => {
+            if (element.code == 'DuplicateUserName')
+              this.toastr.error('Username is already taken', 'Registration failed.');
+          });
+        else
+          this.toastr.error('Registration failed.');
+      }
+    );
+  }
 }
