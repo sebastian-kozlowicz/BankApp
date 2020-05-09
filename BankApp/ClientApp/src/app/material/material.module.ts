2c407036d5468b570c1fefa2d17b764bc433d54e
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2020-05-09 14:18:34 +0200

    Add country drop down list

--- BankApp/ClientApp/src/app/material/material.module.ts
+++ BankApp/ClientApp/src/app/material/material.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 const MaterialComponents = [
   MatButtonModule,
   MatStepperModule,
-  MatInputModule
+  MatInputModule,
+  MatSelectModule
 ];
 
 @NgModule({
