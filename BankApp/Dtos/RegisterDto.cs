9993a73468df04d9bdf0860939f079af03e01ff5
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2020-05-15 22:25:42 +0200

    Add PhoneNumber field in RegisterDto

--- BankApp/Dtos/RegisterDto.cs
+++ BankApp/Dtos/RegisterDto.cs
@@ -20,6 +20,12 @@ namespace BankApp.Dtos
         public string Email { get; set; }
 
         [Required]
+        [StringLength(12, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 8)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
