5a14658435430e729253c4bb714f12221e36d655
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2020-05-17 19:49:36 +0200

    Add AccountType and Currency columns in BankAccounts table

--- BankApp/Models/BankAccount.cs
+++ BankApp/Models/BankAccount.cs
@@ -1,8 +1,12 @@
-﻿namespace BankApp.Models
+﻿using BankApp.Enumerators;
+
+namespace BankApp.Models
 {
     public class BankAccount
     {
         public int Id { get; set; }
+        public AccountType AccountType { get; set; }
+        public Currency Currency { get; set; }
         public string Number { get; set; }
         public decimal Balance { get; set; }
         public ApplicationUser ApplicationUser { get; set; }
