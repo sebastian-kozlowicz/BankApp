6f1c17ae2c14fb53ca9b7c94fea1b06187c67e7e
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2020-05-25 22:21:23 +0200

    Create map from ReigsterDto to ApplicationUser

--- BankApp/Mapping/MappingProfile.cs
+++ BankApp/Mapping/MappingProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using BankApp.Dtos.Administrator;
 using BankApp.Dtos.ApplicationUser;
+using BankApp.Dtos.Auth;
 using BankApp.Dtos.BankAccount;
 using BankApp.Dtos.Card;
 using BankApp.Dtos.Customer;
@@ -21,6 +22,18 @@ namespace BankApp.Mapping
             CreateMap<Customer, CustomerDto>();
             CreateMap<Employee, EmployeeDto>();
             CreateMap<Manager, ManagerDto>();
+            CreateMap<RegisterDto, ApplicationUser>()
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.User.Email))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.User.Email))
+                .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.User.PhoneNumber))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.User.Name))
+                .ForMember(dest => dest.Surname, opt => opt.MapFrom(src => src.User.Surname))
+                .ForPath(dest => dest.Address.Country, opt => opt.MapFrom(src => src.Address.Country))
+                .ForPath(dest => dest.Address.City, opt => opt.MapFrom(src => src.Address.City))
+                .ForPath(dest => dest.Address.Street, opt => opt.MapFrom(src => src.Address.Street))
+                .ForPath(dest => dest.Address.HouseNumber, opt => opt.MapFrom(src => src.Address.HouseNumber))
+                .ForPath(dest => dest.Address.ApartmentNumber, opt => opt.MapFrom(src => src.Address.ApartmentNumber))
+                .ForPath(dest => dest.Address.PostalCode, opt => opt.MapFrom(src => src.Address.PostalCode));
         }
     }
 }
