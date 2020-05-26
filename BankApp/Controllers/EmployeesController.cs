11f951ae67337c77c7b8a49ca7eee1c2ac12243d
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2020-05-26 21:55:53 +0200

    Add using automapper for ApplicationUser creation in user creation actions

--- BankApp/Controllers/EmployeesController.cs
+++ BankApp/Controllers/EmployeesController.cs
@@ -34,10 +34,8 @@ namespace BankApp.Controllers
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
 
-            var user = new ApplicationUser { UserName = model.User.Email, Email = model.User.Email, PhoneNumber = model.User.PhoneNumber, Name = model.User.Name, Surname = model.User.Surname };
-
+            var user = _mapper.Map<ApplicationUser>(model);
             user.Employee = new Employee() { Id = user.Id };
-            user.Address = new Address() { Id = user.Id, Country = model.Address.Country, City = model.Address.City, Street = model.Address.Street, HouseNumber = model.Address.HouseNumber, ApartmentNumber = model.Address.ApartmentNumber, PostalCode = model.Address.PostalCode };
 
             var result = await _userManager.CreateAsync(user, model.User.Password);
 
