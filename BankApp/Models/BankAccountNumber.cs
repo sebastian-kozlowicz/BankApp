61759bfb4c7f8edf90121bd5ba91ba0213fe2378
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2020-06-04 21:27:14 +0200

    Add BankAccountNumber class

---
+++ BankApp/Models/BankAccountNumber.cs
@@ -0,0 +1,14 @@
+﻿namespace BankApp.Models
+{
+    public class BankAccountNumber
+    {
+        public string CountryCode { get; set; }
+        public int CheckNumber { get; set; }
+        public int NationalBankCode { get; set; }
+        public int BranchCode { get; set; }
+        public int NationalCheckDigit { get; set; }
+        public long AccountNumber { get; set; }
+        public string AccountNumberText { get; set; }
+        public string Iban { get; set; }
+    }
+}
