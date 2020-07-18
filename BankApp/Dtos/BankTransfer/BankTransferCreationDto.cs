d42fa71c14fffdcb3cd23fa8fcefa3ddf11d9a8a
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2020-07-18 22:07:53 +0200

    Add BankTransferCreationDto

---
+++ BankApp/Dtos/BankTransfer/BankTransferCreationDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankApp.Dtos.BankTransfer
+{
+    public class BankTransferCreationDto
+    {
+        [Required]
+        public int RequesterBankAccountId { get; set; }
+        [Required]
+        public string ReceiverIban { get; set; }
+        [Required]
+        public decimal Value { get; set; }
+    }
+}
