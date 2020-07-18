a5a1b4c558c0adf56c710e1dff56a39b3194b1d8
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2020-07-18 21:49:36 +0200

    Add BankTransferController draft

---
+++ BankApp/Controllers/BankTransferController.cs
@@ -0,0 +1,23 @@
+﻿using BankApp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BankTransferController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BankTransferController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public ActionResult CreateBankTransfer()
+        {
+            return Ok();
+        }
+    }
+}
