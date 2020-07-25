a8a95e21ad20f84708edbbdd3776847876e55add
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2020-07-25 23:25:14 +0200

    Add validation to Value property in BankTransferCreationDto

--- BankApp/Dtos/BankTransfer/BankTransferCreationDto.cs
+++ BankApp/Dtos/BankTransfer/BankTransferCreationDto.cs
@@ -9,6 +9,8 @@ namespace BankApp.Dtos.BankTransfer
         [Required]
         public string ReceiverIban { get; set; }
         [Required]
+        [RegularExpression(@"^\d+.?\d{0,2}$", ErrorMessage = "The {0} is accepting maximum two decimal points")]
+        [Range(typeof(decimal), "0,01", "9999999999999999,99", ErrorMessage = "The {0} must be at least {1} and at max {2} value")]
         public decimal? Value { get; set; }
     }
 }
