8baba1719ed635f9b2e2d2dae92654bdf3e2e9bb
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2020-07-29 17:41:10 +0200

    Add ITransferService interface

---
+++ BankApp/Interfaces/ITransferService.cs
@@ -0,0 +1,9 @@
+﻿using BankApp.Models;
+
+namespace BankApp.Interfaces
+{
+    public interface ITransferService<T> where T : class, ITransferService<T>
+    {
+        public void Create(BankAccount bankAccount, BankAccount targetBankAccount, decimal value);
+    }
+}
