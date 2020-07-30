43c4e5107e212301b91d39ee02cffa42be0c5091
Sebastian Kozlowicz <sebastian.kozlowicz@hotmail.com>
2020-07-30 14:41:05 +0200

    Annotate types of parameters in BankAccountService methods

--- BankApp/ClientApp/src/app/services/bank-account.service.ts
+++ BankApp/ClientApp/src/app/services/bank-account.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BankAccountWithCustomerCreation } from "../interfaces/bank-account/with-customer/bank-account-with-customer-creation";
+import { BankAccountCreation } from "../interfaces/bank-account/bank-account-creation";
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +12,11 @@ export class BankAccountService {
 
   private readonly bankAccountEndpoint = '/api/bankaccounts';
 
-  createBankAccount(bankAccount) {
+  createBankAccount(bankAccount: BankAccountCreation) {
     return this.http.post(this.bankAccountEndpoint, bankAccount);
   }
 
-  createBankAccountWithCustomer(bankAccountWithCustomer) {
+  createBankAccountWithCustomer(bankAccountWithCustomer: BankAccountWithCustomerCreation) {
     return this.http.post(this.bankAccountEndpoint + '/create-with-customer', bankAccountWithCustomer);
   }
 }
