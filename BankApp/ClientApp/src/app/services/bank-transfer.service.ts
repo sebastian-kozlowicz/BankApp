883f51ce49b8577e43dc44c3c27eee9bf78c8592
Sebastian Kozlowicz <sebastian.kozlowicz@hotmail.com>
2020-07-30 14:34:41 +0200

    Add BankTransfer service on client side

---
+++ BankApp/ClientApp/src/app/services/bank-transfer.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BankTransferService {
+
+  constructor(private http: HttpClient) { }
+
+  private readonly bankBankTransfer = '/api/banktransfers';
+
+  createBankTransfer(bankTransfer) {
+    return this.http.post(this.bankBankTransfer, bankTransfer);
+  }
+}
