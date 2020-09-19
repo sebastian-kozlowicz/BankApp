ed03a8e0bf7cee2b3a50f2483392575f9a84d15b
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2020-09-19 12:35:20 +0200

    Add BranchesController

---
+++ BankApp/Controllers/BranchesController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using BankApp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BranchesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public BranchesController(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public ActionResult CreateBranch()
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok();
+        }
+    }
+}
