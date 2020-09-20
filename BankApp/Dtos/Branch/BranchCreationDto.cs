5f9ee4857154e84819ff50b488c8298b0148f1bc
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2020-09-20 22:37:17 +0200

    Add BranchCode regex validation

--- BankApp/Dtos/Branch/BranchCreationDto.cs
+++ BankApp/Dtos/Branch/BranchCreationDto.cs
@@ -5,6 +5,7 @@ namespace BankApp.Dtos.Branch
     public class BranchCreationDto
     {
         [Required]
+        [RegularExpression(@"^\d{3}$", ErrorMessage = "String length can only contain digits and must be of length 3 characters.")]
         public string BranchCode { get; set; }
     }
 }
