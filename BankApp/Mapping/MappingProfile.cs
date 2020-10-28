d29bc81571c07ba74221c044a50b89a7cc4b04c2
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2020-10-28 18:43:57 +0100

    Add mapping from RegisterByAnotherUserDto to ApplicationUser

--- BankApp/Mapping/MappingProfile.cs
+++ BankApp/Mapping/MappingProfile.cs
@@ -41,6 +41,20 @@ namespace BankApp.Mapping
                 .ForPath(dest => dest.UserAddress.PostalCode, opt => opt.MapFrom(src => src.Address.PostalCode))
                 .AfterMap((src, dest) => dest.UserAddress.Id = dest.Id);
 
+            CreateMap<RegisterByAnotherUserDto, ApplicationUser>()
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.User.Email))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.User.Email))
+                .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.User.PhoneNumber))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.User.Name))
+                .ForMember(dest => dest.Surname, opt => opt.MapFrom(src => src.User.Surname))
+                .ForPath(dest => dest.UserAddress.Country, opt => opt.MapFrom(src => src.Address.Country))
+                .ForPath(dest => dest.UserAddress.City, opt => opt.MapFrom(src => src.Address.City))
+                .ForPath(dest => dest.UserAddress.Street, opt => opt.MapFrom(src => src.Address.Street))
+                .ForPath(dest => dest.UserAddress.HouseNumber, opt => opt.MapFrom(src => src.Address.HouseNumber))
+                .ForPath(dest => dest.UserAddress.ApartmentNumber, opt => opt.MapFrom(src => src.Address.ApartmentNumber))
+                .ForPath(dest => dest.UserAddress.PostalCode, opt => opt.MapFrom(src => src.Address.PostalCode))
+                .AfterMap((src, dest) => dest.UserAddress.Id = dest.Id);
+
             CreateMap<BranchWithAddressCreationDto, Branch>()
               .ForMember(dest => dest.BranchCode, opt => opt.MapFrom(src => src.Branch.BranchCode))
               .ForPath(dest => dest.BranchAddress.Country, opt => opt.MapFrom(src => src.Address.Country))
