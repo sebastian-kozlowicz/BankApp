42e855d0577132183f3a729bc30243178eba8d95
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2020-10-30 21:34:40 +0100

    Add UserCreationByAnotherUser interface on client side

---
+++ BankApp/ClientApp/src/app/interfaces/user/user-creation-by-another-user.js
@@ -0,0 +1,3 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//# sourceMappingURL=user-creation-by-another-user.js.map
---
+++ BankApp/ClientApp/src/app/interfaces/user/user-creation-by-another-user.ts
@@ -0,0 +1,6 @@
+export interface UserCreationByAnotherUser {
+  name: string;
+  surname: string;
+  email: string;
+  phoneNumber: string;
+}
