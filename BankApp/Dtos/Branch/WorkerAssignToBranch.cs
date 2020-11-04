2e4fcd2039cd1fe8f17b1e7e36e7e999333e720e
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2020-11-04 19:23:48 +0100

    Add WorkerAssignToBranch dto

---
+++ BankApp/Dtos/Branch/WorkerAssignToBranch.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankApp.Dtos.Branch
+{
+    public class WorkerAssignToBranch
+    {
+        [Required]
+        public int WorkerId { get; set; }
+        [Required]
+        public int BranchId { get; set; }
+    }
+}
