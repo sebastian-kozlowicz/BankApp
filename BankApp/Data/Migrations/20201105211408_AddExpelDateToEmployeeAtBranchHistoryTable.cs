daf1ba80f1f5872221bdd1d6718654c61ba7b9c1
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2020-11-05 22:15:44 +0100

    Add ExpelDate column to EmployeeAtBranchHistory table

---
+++ BankApp/Data/Migrations/20201105211408_AddExpelDateToEmployeeAtBranchHistoryTable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BankApp.Data.Migrations
+{
+    public partial class AddExpelDateToEmployeeAtBranchHistoryTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExpelDate",
+                table: "EmployeeAtBranchHistory",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpelDate",
+                table: "EmployeeAtBranchHistory");
+        }
+    }
+}
