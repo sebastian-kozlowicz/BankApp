97e17cce7a320f4c862f11040f16260daee17f48
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2020-11-05 22:13:05 +0100

    Add ExpelDate column to ManagerAtBranchHistory table

---
+++ BankApp/Data/Migrations/20201105211051_AddExpelDateToManagerAtBranchHistoryTable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BankApp.Data.Migrations
+{
+    public partial class AddExpelDateToManagerAtBranchHistoryTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExpelDate",
+                table: "ManagerAtBranchHistory",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpelDate",
+                table: "ManagerAtBranchHistory");
+        }
+    }
+}
--- BankApp/Models/ManagerAtBranchHistory.cs
+++ BankApp/Models/ManagerAtBranchHistory.cs
@@ -6,6 +6,7 @@ namespace BankApp.Models
     {
         public int Id { get; set; }
         public DateTime AssignDate { get; set; }
+        public DateTime? ExpelDate { get; set; }
         public Branch Branch { get; set; }
         public int BranchId { get; set; }
         public Manager Manager { get; set; }
