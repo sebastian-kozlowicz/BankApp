1a1a67d9812e4dd4b7dde8dc87b30a1898202883
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2020-11-08 19:04:34 +0100

    Add BankAccountWithCustomerCreationByAnotherUserDto

---
+++ BankApp/Dtos/BankAccount/WithCustomer/BankAccountWithCustomerCreationByAnotherUserDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using BankApp.Dtos.Auth;
+
+namespace BankApp.Dtos.BankAccount.WithCustomer
+{
+    public class BankAccountWithCustomerCreationByAnotherUserDto
+    {
+        [Required] 
+        public RegisterByAnotherUserDto Register { get; set; }
+        [Required] 
+        public BankAccountCreationDto BankAccount { get; set; }
+    }
+}
