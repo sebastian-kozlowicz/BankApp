abbfff6e3b0db8ff8c6bae6ffe00ac3ac5c654f2
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2020-11-11 20:39:05 +0100

    Add BranchesControllerTests class

---
+++ BankApp.UnitTests/Controllers/BranchesControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using AutoMapper;
+using BankApp.Controllers;
+using BankApp.Data;
+using BankApp.Mapping;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankApp.UnitTests.Controllers
+{
+    [TestClass]
+    public class BranchesControllerTests
+    {
+        private BranchesController _branchesController;
+        private readonly IMapper _mapper = new MapperConfiguration(c => c.AddProfile<MappingProfile>()).CreateMapper();
+        private ApplicationDbContext _context;
+
+        private static ApplicationDbContext GetMockContext()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            var context = new ApplicationDbContext(options);
+
+            context.SaveChanges();
+
+            return context;
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _context = GetMockContext();
+            _branchesController = new BranchesController(_context, _mapper);
+        }
+    }
+}
