1613f63d4f6f09dec9703164b5eff189aac85d14
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2020-11-29 17:15:59 +0100

    Refactor models by implement inheritance

--- BankApp/Models/TellerAtBranchHistory.cs
+++ BankApp/Models/TellerAtBranchHistory.cs
@@ -1,19 +1,8 @@
-﻿using System;
-
-namespace BankApp.Models
+﻿namespace BankApp.Models
 {
-    public class TellerAtBranchHistory
+    public class TellerAtBranchHistory : WorkerAtBranchHistory
     {
-        public int Id { get; set; }
-        public DateTime AssignDate { get; set; }
-        public DateTime? ExpelDate { get; set; }
-        public Branch Branch { get; set; }
-        public int BranchId { get; set; }
         public Teller Teller { get; set; }
         public int TellerId { get; set; }
-        public ApplicationUser AssignedBy { get; set; }
-        public int AssignedById { get; set; }
-        public ApplicationUser ExpelledBy { get; set; }
-        public int? ExpelledById { get; set; }
     }
 }
---
+++ BankApp/Models/WorkerAtBranchHistory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BankApp.Models
+{
+    public abstract class WorkerAtBranchHistory
+    {
+        public int Id { get; set; }
+        public DateTime AssignDate { get; set; }
+        public DateTime? ExpelDate { get; set; }
+        public Branch Branch { get; set; }
+        public int BranchId { get; set; }
+        public ApplicationUser AssignedBy { get; set; }
+        public int AssignedById { get; set; }
+        public ApplicationUser ExpelledBy { get; set; }
+        public int? ExpelledById { get; set; }
+    }
+}
