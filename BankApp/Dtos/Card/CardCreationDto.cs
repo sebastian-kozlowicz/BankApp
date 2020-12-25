9014a1272470a89a830a0208da21152e4f3b5d79
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2020-12-25 20:35:31 +0100

    Add CardCreationDto

---
+++ BankApp/Dtos/Card/CardCreationDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankApp.Dtos.Card
+{
+    public class CardCreationDto
+    {
+        [Required]
+        public int? BankAccountId { get; set; }
+    }
+}
