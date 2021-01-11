7d12ea582f7f88287f65ab04d671a0ad55e4c9a5
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2021-01-11 22:17:43 +0100

    Add draft GenerateCardNumber mehod in CardNumberFactory

--- BankApp/Helpers/Factories/CardNumberFactory.cs
+++ BankApp/Helpers/Factories/CardNumberFactory.cs
@@ -1,8 +1,13 @@
 ﻿using BankApp.Interfaces;
+using System;
 
 namespace BankApp.Helpers.Factories
 {
     public class CardNumberFactory : ICardNumberFactory
     {
+        public string GenerateCardNumber()
+        {
+            return Guid.NewGuid().ToString().Substring(0, 15);
+        }
     }
 }
