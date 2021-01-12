2992990e7ec3556942cd95235f5d12ec2f36fd09
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2021-01-12 23:03:39 +0100

    Add columns in cards table

---
+++ BankApp/Data/Migrations/20210112220029_AddColumnsInCardsTable.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BankApp.Data.Migrations
+{
+    public partial class AddColumnsInCardsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "AccountIdentificationNumber",
+                table: "Cards",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<byte>(
+                name: "CheckDigit",
+                table: "Cards",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "IssuerIdentificationNumber",
+                table: "Cards",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<byte>(
+                name: "MajorIndustryIdentifier",
+                table: "Cards",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AccountIdentificationNumber",
+                table: "Cards");
+
+            migrationBuilder.DropColumn(
+                name: "CheckDigit",
+                table: "Cards");
+
+            migrationBuilder.DropColumn(
+                name: "IssuerIdentificationNumber",
+                table: "Cards");
+
+            migrationBuilder.DropColumn(
+                name: "MajorIndustryIdentifier",
+                table: "Cards");
+        }
+    }
+}
--- BankApp/Models/Card.cs
+++ BankApp/Models/Card.cs
@@ -3,6 +3,10 @@
     public class Card
     {
         public int Id { get; set; }
+        public byte MajorIndustryIdentifier { get; set; }
+        public int IssuerIdentificationNumber { get; set; }
+        public long AccountIdentificationNumber { get; set; }
+        public byte CheckDigit { get; set; }
         public string Number { get; set; }
         public BankAccount BankAccount { get; set; }
         public int BankAccountId { get; set; }
