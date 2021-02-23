f1d56b2549561120db6010c1b8d854fdb4ba4be8
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2021-02-23 22:18:02 +0100

    Add Luhn algoritm implementation to generate card number check digit

--- BankApp/Helpers/Builders/PaymentCardNumberBuilder.cs
+++ BankApp/Helpers/Builders/PaymentCardNumberBuilder.cs
@@ -1,4 +1,6 @@
-﻿namespace BankApp.Helpers.Builders
+﻿using System.Linq;
+
+namespace BankApp.Helpers.Builders
 {
     public static class PaymentCardNumberBuilder
     {
@@ -7,6 +9,37 @@
         public static string GetAccountIdentificationNumber(int length, string accountNumberText)
         {
             return accountNumberText.Substring(0, length - BankIdentificationNumberAndCheckDigitLength);
+        }
+
+        public static string GenerateCheckDigit(string number)
+        {
+            var sum = 0;
+            var oddPositon = false;
+            var numberDigitsArray = number.Select(digit => int.Parse(digit.ToString())).ToArray().Reverse();
+
+            foreach (var digit in numberDigitsArray)
+            {
+                if (oddPositon)
+                {
+                    var digitSquared = digit * 2;
+                    var digitSquaredText = digitSquared.ToString();
+
+                    if (digitSquaredText.Length >= 10)
+                    {
+                        var digitSquaredArray = digitSquaredText.Select(digit => int.Parse(digit.ToString())).ToArray();
+                        var digitsSquaredSum = digitSquaredArray.Sum();
+                        sum += digitsSquaredSum;
+                    }
+                }
+                else
+                {
+                    sum += digit;
+                }
+
+                oddPositon = !oddPositon;
+            }
+
+            return ((10 - sum % 10) % 10).ToString();
         }
     }
 }
