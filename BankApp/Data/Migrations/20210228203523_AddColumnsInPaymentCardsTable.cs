22a253176adf359703bc9053b7fa3d718bf81c93
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2021-02-28 21:37:06 +0100

    Add columns in PaymentCards table

---
+++ BankApp/Data/Migrations/20210228203523_AddColumnsInPaymentCardsTable.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BankApp.Data.Migrations
+{
+    public partial class AddColumnsInPaymentCardsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AccountIdentificationNumberText",
+                table: "PaymentCards",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "IssuingNetwork",
+                table: "PaymentCards",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AccountIdentificationNumberText",
+                table: "PaymentCards");
+
+            migrationBuilder.DropColumn(
+                name: "IssuingNetwork",
+                table: "PaymentCards");
+        }
+    }
+}
--- BankApp/Models/PaymentCard.cs
+++ BankApp/Models/PaymentCard.cs
@@ -1,4 +1,6 @@
-﻿namespace BankApp.Models
+﻿using BankApp.Enumerators;
+
+namespace BankApp.Models
 {
     public class PaymentCard
     {
@@ -6,8 +8,10 @@
         public byte MajorIndustryIdentifier { get; set; }
         public int BankIdentificationNumber { get; set; }
         public long AccountIdentificationNumber { get; set; }
+        public string AccountIdentificationNumberText { get; set; }
         public byte CheckDigit { get; set; }
         public string Number { get; set; }
+        public IssuingNetwork IssuingNetwork { get; set; }
         public BankAccount BankAccount { get; set; }
         public int BankAccountId { get; set; }
     }
