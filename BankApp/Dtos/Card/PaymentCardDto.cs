e907629087f7bd372c58f079a64ede92975ca157
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2021-03-07 20:31:34 +0100

    Add missing properties in PaymentCardDto

--- BankApp/Dtos/Card/PaymentCardDto.cs
+++ BankApp/Dtos/Card/PaymentCardDto.cs
@@ -1,8 +1,17 @@
-﻿namespace BankApp.Dtos.Card
+﻿using BankApp.Enumerators;
+
+namespace BankApp.Dtos.Card
 {
     public class PaymentCardDto
     {
         public int Id { get; set; }
+        public byte MajorIndustryIdentifier { get; set; }
+        public int BankIdentificationNumber { get; set; }
+        public long AccountIdentificationNumber { get; set; }
+        public string AccountIdentificationNumberText { get; set; }
+        public byte CheckDigit { get; set; }
         public string Number { get; set; }
+        public IssuingNetwork IssuingNetwork { get; set; }
+        public int BankAccountId { get; set; }
     }
 }
