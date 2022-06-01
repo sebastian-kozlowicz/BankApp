ec0d5d88a3c3b24ed519c927175a435af355fbdd
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2022-06-01 23:19:45 +0200

    Add test

--- BankApp.UnitTests/Helpers/Services/BankAccountServiceTests.cs
+++ BankApp.UnitTests/Helpers/Services/BankAccountServiceTests.cs
@@ -4,7 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using BankApp.Data;
-using BankApp.Dtos.BankAccount;
+using BankApp.Dtos.Address;
+using BankApp.Dtos.ApplicationUser;
+using BankApp.Dtos.Auth;
+using BankApp.Dtos.BankAccount.WithCustomerCreation;
 using BankApp.Enumerators;
 using BankApp.Helpers.Services;
 using BankApp.Interfaces.Helpers.Builders.Number;
@@ -15,6 +18,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using BankAccountCreationDto = BankApp.Dtos.BankAccount.BankAccountCreationDto;
 
 namespace BankApp.UnitTests.Helpers.Services
 {
@@ -192,7 +196,7 @@ namespace BankApp.UnitTests.Helpers.Services
                 CustomerId = (int)bankAccountCreation.CustomerId,
                 Customer = _customerApplicationUser.Customer,
                 CreatedById = (int)bankAccountCreation.CustomerId,
-                CreatedBy = _customerApplicationUser,
+                CreatedBy = _customerApplicationUser
             };
 
             _bankAccountNumberBuilderMock.Setup(anf => anf.GenerateBankAccountNumber(null)).Returns(bankAccountNumber);
@@ -211,6 +215,164 @@ namespace BankApp.UnitTests.Helpers.Services
             bankAccountFromDb.Should().BeEquivalentTo(expectedBankAccount,
                 options => options.Excluding(b => b.OpenedDate));
             bankAccountFromDb.OpenedDate.Should().NotBe(DateTime.MinValue);
+        }
+
+        [TestMethod]
+        public async Task
+            CreateBankAccountWithCustomerByCustomerAsync_Should_CreateBankAccountWithCustomer_And_ReturnBankAccount()
+        {
+            // Arrange
+            var bankAccountCreation = new BankAccountWithCustomerCreationByCustomerDto
+            {
+                Register = new RegisterDto
+                {
+                    User = new ApplicationUserCreationBySameUserDto
+                    {
+                        Name = "John",
+                        Surname = "Smith",
+                        Email = "john@smith.com",
+                        PhoneNumber = "123456789",
+                        Password = "qwerty"
+                    },
+                    Address = new AddressCreationDto
+                    {
+                        Country = "United States",
+                        City = "New York",
+                        Street = "Glenwood Ave",
+                        HouseNumber = "10",
+                        ApartmentNumber = "11",
+                        PostalCode = "10028"
+                    }
+                },
+                BankAccount = new Dtos.BankAccount.WithCustomerCreation.BankAccountCreationDto
+                {
+                    AccountType = AccountType.Checking,
+                    Currency = Currency.Eur
+                }
+            };
+
+            var bankAccountNumber = new BankAccountNumber
+            {
+                CountryCode = "PL",
+                CheckDigits = "61",
+                NationalBankCode = "1080",
+                BranchCode = "000",
+                NationalCheckDigit = 1,
+                AccountNumber = 0,
+                AccountNumberText = "0000000000000000",
+                Iban = "PL61108000010000000000000000",
+                IbanSeparated = "PL 61 1080 0001 0000 0000 0000 0000"
+            };
+
+            var expectedCustomer = new Customer
+            {
+                Id = 5,
+                ApplicationUser = new ApplicationUser
+                {
+                    Id = 5,
+                    Name = bankAccountCreation.Register.User.Name,
+                    Surname = bankAccountCreation.Register.User.Surname,
+                    Email = bankAccountCreation.Register.User.Email,
+                    PhoneNumber = bankAccountCreation.Register.User.PhoneNumber,
+                    UserAddress = new UserAddress
+                    {
+                        Id = 5,
+                        Country = bankAccountCreation.Register.Address.Country,
+                        City = bankAccountCreation.Register.Address.City,
+                        Street = bankAccountCreation.Register.Address.Street,
+                        HouseNumber = bankAccountCreation.Register.Address.HouseNumber,
+                        ApartmentNumber = bankAccountCreation.Register.Address.ApartmentNumber,
+                        PostalCode = bankAccountCreation.Register.Address.PostalCode
+                    }
+                }
+            };
+
+            var expectedBankAccount = new BankAccount
+            {
+                Id = 3,
+                AccountType = (AccountType)bankAccountCreation.BankAccount.AccountType,
+                Currency = (Currency)bankAccountCreation.BankAccount.Currency,
+                CountryCode = bankAccountNumber.CountryCode,
+                CheckDigits = bankAccountNumber.CheckDigits,
+                NationalBankCode = bankAccountNumber.NationalBankCode,
+                BranchCode = bankAccountNumber.BranchCode,
+                NationalCheckDigit = bankAccountNumber.NationalCheckDigit,
+                AccountNumber = bankAccountNumber.AccountNumber,
+                AccountNumberText = bankAccountNumber.AccountNumberText,
+                Iban = bankAccountNumber.Iban,
+                IbanSeparated = bankAccountNumber.IbanSeparated,
+                Balance = 0,
+                DebitLimit = 0,
+                CustomerId = 5,
+                CreatedById = 5
+            };
+
+            _userManagerMock.Setup(m => m.CreateAsync(It.IsAny<ApplicationUser>(), It.IsAny<string>()))
+                .ReturnsAsync(IdentityResult.Success).Callback<ApplicationUser, string>((user, password) =>
+                {
+                    _context.Users.Add(user);
+                    _context.SaveChanges();
+                });
+
+            _bankAccountNumberBuilderMock.Setup(anf => anf.GenerateBankAccountNumber(null)).Returns(bankAccountNumber);
+
+            // Act
+            var result = await _sut.CreateBankAccountWithCustomerByCustomerAsync(bankAccountCreation);
+
+            // Assert
+            _userManagerMock.Verify(m => m.AddToRoleAsync(It.IsAny<ApplicationUser>(), UserRole.Customer.ToString()),
+                Times.Once);
+
+            result.Should().NotBeNull();
+            result.Should().BeEquivalentTo(expectedBankAccount,
+                options => options.Excluding(b => b.OpenedDate).Excluding(b => b.Customer).Excluding(b => b.CreatedBy));
+            result.OpenedDate.Should().NotBe(DateTime.MinValue);
+            result.Customer.Id.Should().Be(expectedCustomer.Id);
+            result.Customer.ApplicationUser.Id.Should().Be(expectedCustomer.Id);
+            result.Customer.ApplicationUser.Name.Should().Be(expectedCustomer.ApplicationUser.Name);
+            result.Customer.ApplicationUser.Surname.Should().Be(expectedCustomer.ApplicationUser.Surname);
+            result.Customer.ApplicationUser.Email.Should().Be(expectedCustomer.ApplicationUser.Email);
+            result.Customer.ApplicationUser.PhoneNumber.Should().Be(expectedCustomer.ApplicationUser.PhoneNumber);
+            result.Customer.ApplicationUser.UserAddress.Id.Should().Be(expectedCustomer.ApplicationUser.UserAddress.Id);
+            result.Customer.ApplicationUser.UserAddress.Country.Should()
+                .Be(expectedCustomer.ApplicationUser.UserAddress.Country);
+            result.Customer.ApplicationUser.UserAddress.City.Should()
+                .Be(expectedCustomer.ApplicationUser.UserAddress.City);
+            result.Customer.ApplicationUser.UserAddress.Street.Should()
+                .Be(expectedCustomer.ApplicationUser.UserAddress.Street);
+            result.Customer.ApplicationUser.UserAddress.HouseNumber.Should()
+                .Be(expectedCustomer.ApplicationUser.UserAddress.HouseNumber);
+            result.Customer.ApplicationUser.UserAddress.ApartmentNumber.Should()
+                .Be(expectedCustomer.ApplicationUser.UserAddress.ApartmentNumber);
+            result.Customer.ApplicationUser.UserAddress.PostalCode.Should()
+                .Be(expectedCustomer.ApplicationUser.UserAddress.PostalCode);
+
+            var bankAccountFromDb = _context.BankAccounts.SingleOrDefault(ba => ba.Id == result.Id);
+            bankAccountFromDb.Should().NotBeNull();
+            bankAccountFromDb.Should().BeEquivalentTo(expectedBankAccount,
+                options => options.Excluding(b => b.OpenedDate).Excluding(b => b.Customer).Excluding(b => b.CreatedBy));
+            bankAccountFromDb.OpenedDate.Should().NotBe(DateTime.MinValue);
+            bankAccountFromDb.Customer.Id.Should().Be(expectedCustomer.Id);
+            bankAccountFromDb.Customer.ApplicationUser.Id.Should().Be(expectedCustomer.Id);
+            bankAccountFromDb.Customer.ApplicationUser.Name.Should().Be(expectedCustomer.ApplicationUser.Name);
+            bankAccountFromDb.Customer.ApplicationUser.Surname.Should().Be(expectedCustomer.ApplicationUser.Surname);
+            bankAccountFromDb.Customer.ApplicationUser.Email.Should().Be(expectedCustomer.ApplicationUser.Email);
+            bankAccountFromDb.Customer.ApplicationUser.PhoneNumber.Should()
+                .Be(expectedCustomer.ApplicationUser.PhoneNumber);
+            bankAccountFromDb.Customer.ApplicationUser.UserAddress.Id.Should()
+                .Be(expectedCustomer.ApplicationUser.UserAddress.Id);
+            bankAccountFromDb.Customer.ApplicationUser.UserAddress.Country.Should()
+                .Be(expectedCustomer.ApplicationUser.UserAddress.Country);
+            bankAccountFromDb.Customer.ApplicationUser.UserAddress.City.Should()
+                .Be(expectedCustomer.ApplicationUser.UserAddress.City);
+            bankAccountFromDb.Customer.ApplicationUser.UserAddress.Street.Should()
+                .Be(expectedCustomer.ApplicationUser.UserAddress.Street);
+            bankAccountFromDb.Customer.ApplicationUser.UserAddress.HouseNumber.Should()
+                .Be(expectedCustomer.ApplicationUser.UserAddress.HouseNumber);
+            bankAccountFromDb.Customer.ApplicationUser.UserAddress.ApartmentNumber.Should()
+                .Be(expectedCustomer.ApplicationUser.UserAddress.ApartmentNumber);
+            bankAccountFromDb.Customer.ApplicationUser.UserAddress.PostalCode.Should()
+                .Be(expectedCustomer.ApplicationUser.UserAddress.PostalCode);
         }
     }
 }
